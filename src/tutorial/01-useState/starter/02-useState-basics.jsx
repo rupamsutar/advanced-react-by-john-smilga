8aaded7ddb1dfda8c2e0603e277269f7c2a142a7
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-04 16:10:55 +0530

    UseState Basics

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+ 
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState("Rupam"));
+  // console.log(useState("Rupam")[]);
+  // const value = useState("rupam")[0];
+  // console.log(value);
+  // const fun = useState("rupam")[1];
+  // console.log(fun)
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h4>You Clicked {count} times</h4>
+      <button type="button" className="btn" onClick={handleClick}>Increase</button>
+    </div>
+  )
 };
+
+
 
 export default UseStateBasics;
