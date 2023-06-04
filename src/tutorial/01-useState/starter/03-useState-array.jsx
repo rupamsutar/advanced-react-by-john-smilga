246a55bf1d9091a326f3a75dc61ad43e8a8ce406
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-04 16:34:28 +0530

    Challenge Complete

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [newData, setNewData] = useState(data);
+
+  const removeHandler = (id) => {
+    setNewData(newData.filter((item) => item.id !== id))
+  }
+
+  const clearHandler = () => {
+    setNewData([]);
+  }
+
+  return (
+    <>
+    {newData.map((item) => {
+      return(
+        <div key={item.id} className="">
+          <h4>{item.name}</h4>
+          <button type="button" className="btn" onClick={() => {
+            removeHandler(item.id);
+          }}>remove</button>
+        </div>
+      )
+    })}
+
+    <button onClick={clearHandler}>Clear List</button>
+    </>
+  );
 };
 
 export default UseStateArray;
